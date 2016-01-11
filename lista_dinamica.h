d3283011edc2e51bb743b96c0909a826c96d68c1
rlbernardes <rafaelbernardes0@gmail.com>
2016-01-11 08:17:03 -0200

    Update and rename .gitignore to lista_dinamica.h

---
+++ lista_dinamica.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct aluno{
+    int matricula;
+    char nome[50];
+    float n1, n2, n3;
+};
+
+typedef struct elemento *Lista;
+
+
+Lista *cria_lista();
+
+void libera_lista(Lista *li);
+
+int tamanho_lista(Lista *li);
+
+int lista_vazia(Lista *li);
+
+int insere_inicio_lista(Lista *li, struct aluno al);
+
+int insere_final_lista(Lista *li, struct aluno al);
+
+int insere_meio_lista(Lista *li, struct aluno al);
+
+int remove_inicio_lista(Lista *li);
+
+int remove_final_lista(Lista *li);
+
+int remove_meio_lista(Lista *li, int matricula);
+
+void Preenche(struct aluno *al);
+
+void consulta_aluno(Lista *li);
